1c8c61f22c549a32b16686b21673babd9c60d08b
Jujux <jdeneux@gmail.com>
2016-11-23 09:01:01 +0000

    first commit

---
+++ src/testApi/Controllers/BlogController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using testApi.Models;
+
+namespace testApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class BlogsController : Controller
+    {
+
+        private BloggingContext _context;
+        ILogger<BlogsController> _logger;
+
+        public BlogsController(BloggingContext context, ILogger<BlogsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Blog> Index()
+        {
+            return _context.Blog.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetBlog")]
+        public IActionResult GetById(int id)
+        {
+            var result = _context.Blog.SingleOrDefault(blog => blog.BlogId == id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return new ObjectResult(result);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                _logger.LogDebug(@"The model is valid");
+
+                try
+                {
+                    _context.Blog.Add(blog);
+                    _context.SaveChanges();
+                } catch (Exception e)
+                {
+                    _logger.LogDebug(e.ToString());
+                    return BadRequest();
+                }
+                
+            } else
+            {
+                _logger.LogDebug(@"The model is not valid");
+                return BadRequest();
+            }
+
+            return CreatedAtRoute("GetBlog", new { id = blog.BlogId }, blog);
+        }
+
+    }
+}
---
+++ src/testApi/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.Swagger.Model;
+using Microsoft.Extensions.PlatformAbstractions;
+using System.IO;
+
+using testApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace testApi
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Setup the db
+            var connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<BloggingContext>(options => options.UseSqlServer(connection));
+
+            // Add framework services.
+            services.AddMvc();
+
+            services.AddSwaggerGen(options => {
+                options.SingleApiVersion(new Info
+                {
+                    Version = "v1",
+                    Title = "ToDo API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact { Name = "Julien Deneux", Email = "jdeneux@gmail.com", Url = "http://www.jdeneux.com" },
+                    License = new License { Name = "Use under LICX", Url = "http://url.com" }
+                });
+
+                //Determine base path for the application.
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+
+                //Set the comments path for the swagger json and ui.
+                var xmlPath = Path.Combine(basePath, "TestApi.xml");
+                options.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            /*Enabling swagger file*/
+            app.UseSwagger();
+
+            /*Enabling Swagger ui, consider doing it on Development env only*/
+            app.UseSwaggerUi();
+
+            app.UseMvc();
+        }
+    }
+}
