8c44dca9353c3d5ed2dae13fbceb540616ae08c8
Jujux <jdeneux@gmail.com>
2016-11-23 09:37:05 +0000

    Add condenser service manager

--- src/testApi/Program.cs
+++ src/testApi/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
+using CondenserDotNet.Client;
 
 namespace testApi
 {
@@ -12,8 +13,15 @@ namespace testApi
     {
         public static void Main(string[] args)
         {
+            var serviceManager = new ServiceManager("TestService");
+            serviceManager.AddHttpHealthCheck("health", 10)
+                .AddApiUrl("/api/someObject")
+                .AddApiUrl("/api/someOtherObject")
+                .RegisterServiceAsync();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls($"http://*:{serviceManager.ServicePort}")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
